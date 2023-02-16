2eec1e0ee232f7ecd7d01bbff2670f7f2af05120
MekhiHart <mekhihartdelacruz@gmail.com>
2023-02-16 10:45:17 -0800

    Made changes to header, added a new font for BeachHacks link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,12 @@
 export default function Header(){
 
+    const logo = require("./images/BH_7-0.png")
+    const BeachHacks_link = "https://beachhacks.com/"
+
     return(
         <div className="header">
-            <h1>This is the header</h1>
+            <img src={logo} className="header--logo"/>
+            <a href={BeachHacks_link}> BeachHacks</a>
         </div>
     )
 }
