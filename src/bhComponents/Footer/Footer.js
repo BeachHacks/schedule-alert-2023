522b20a192b2f5380b7ca93244771b7bd3fc7fdf
MekhiHart <mekhihartdelacruz@gmail.com>
2023-02-18 13:33:16 -0800

    changed class --> classNames in footer component

--- src/bhComponents/Footer/Footer.js
+++ src/bhComponents/Footer/Footer.js
@@ -4,9 +4,9 @@ const Footer = () => {
     return ( 
     <footer>
         <div className = "social">
-            <a href="https://www.instagram.com/beachhackscsulb/" target="blank"><i class="fab fa-instagram"></i></a>&nbsp;&nbsp;
-            <a href="https://twitter.com/beachhackscsulb" target="blank"><i class="fab fa-twitter"></i></a>&nbsp;&nbsp;
-            <a href="https://www.linkedin.com/company/beachhacks/" target="blank"><i class="fab fa-linkedin"></i></a>
+            <a href="https://www.instagram.com/beachhackscsulb/" target="blank"><i className="fab fa-instagram"></i></a>&nbsp;&nbsp;
+            <a href="https://twitter.com/beachhackscsulb" target="blank"><i className="fab fa-twitter"></i></a>&nbsp;&nbsp;
+            <a href="https://www.linkedin.com/company/beachhacks/" target="blank"><i className="fab fa-linkedin"></i></a>
         </div>
         <hr className="footer-divider" />   
         <div className="footer-bottom">
